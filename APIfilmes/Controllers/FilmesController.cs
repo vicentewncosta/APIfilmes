1cbabef42635b895e93bd3df6b2193761bf93745
vicentewncosta <vicentecoosta55@gmail.com>
2023-02-02 11:36:44 -0300

    Critério de busca por id específico

--- APIfilmes/Controllers/FilmesController.cs
+++ APIfilmes/Controllers/FilmesController.cs
@@ -9,14 +9,29 @@ namespace APIfilmes.Controllers;
     public class FilmesController : ControllerBase
     {
 
-        private static List<Filme> filmes = new List<Filme>();
+        private static List<Filme> filme = new List<Filme>();
+        private static int id = 0;
 
         [HttpPost] 
         public void AdicionaFilme([FromBody] Filme filme)
         {
-            filmes.Add(filme);
+
+            filme.Id = id++;
+            FilmesController.filme.Add(filme);
             Console.WriteLine(filme.Titulo);
-            Console.WriteLine(filme.Duracao);
             Console.WriteLine(filme.Genero);
+            Console.WriteLine(filme.Duracao);
+        }
+
+        [HttpGet]
+        public IEnumerable<Filme> RecuperaFilmes()
+        {
+            return filme;
+        }
+
+        [HttpGet("{id}")]
+        public Filme? RecuperaFilmePorId(int id)
+        {
+            return filme.FirstOrDefault(filme => filme.Id == id);
         }
     }
